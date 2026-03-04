221bbdee95e21d099a6a41d4b0ea8e6cabff2f66
Phạm Quốc Tự <quoctu.se@gmail.com>
2026-03-04 23:53:31 +0700

    add connection to MongoDB Atlas

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index", { username: req.query.username || "Phạm Tự", title: "Trang Chủ" });
+mongoose
+  .connect(
+    process.env.MONGODB_URI
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+const MessageSchema = new mongoose.Schema({
+  text: String,
+  createdAt: { type: Date, default: Date.now },
 });
-const PORT = process.env.PORT || 3000;
+
+const Message = mongoose.model("Message", MessageSchema);
+
+app.set("view engine", "ejs");
+app.get("/", async (req, res) => {
+  const message = await Message.findOne().sort({ createdAt: -1 });
+  res.render("index", {
+    username: req.query.username || "Phạm Tự",
+    title: "Trang Chủ",
+    message: message,
+  });
+});
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
