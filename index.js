54234510cc2ec94dd7599df308ca16f32445a0f5
Phạm Quốc Tự <quoctu.se@gmail.com>
2026-03-04 22:17:39 +0700

    change a little code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.send("Demo deployment backend nodejs express to Render!");
+  res.render("index", { username: req.query.username || "Phạm Tự", title: "Trang Chủ" });
 });
 const PORT = process.env.PORT || 3000;
 
